6bbbe907d6f470d4f6c426b2b82a5323515ada04
Heriberto Basulto <basulto93@live.com>
2016-08-25 12:28:56 -0700

    Few adjustments

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace _13_TopSpotsAPI
 {
@@ -10,8 +12,12 @@ namespace _13_TopSpotsAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            var policy = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(policy);
             // Web API routes
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
--- Models/TopSpot.cs
+++ Models/TopSpot.cs
@@ -7,7 +7,6 @@ namespace _13_TopSpotsAPI.Models
 {
     public class TopSpot
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public double[] Location { get; set; }
